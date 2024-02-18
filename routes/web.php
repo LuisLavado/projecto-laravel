b722432ba9c2ef08f0e18117b3bf1098b3125e6b
Luis GitHub <luisito.ingenermax@gmail.com>
2024-02-17 23:18:41 -0500

    Views en Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route::get      => consultar
- * Route::post     => Guardar
- * Route::delete   => Eliminar
- * Route::put      => Actulizar
- * 
- */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/blog', function () {
-    return "Bienvenido a mi Blog";
+    // consulta a la base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+
+    return view('blog', ['posts' => $posts]);
 });
 
 Route::get('/blog/{word}', function ($word) {
-    return $word;
+    // consulta a la base de datos
+    $post = $word;
+    return view('post', ['post' => $post]);
 });
 
-Route::get('/buscar', function (Request $request) {
-    return $request->all();
-});
