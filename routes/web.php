66b3e5f112c575377f590f1761458a9c5b819d50
Luis GitHub <luisito.ingenermax@gmail.com>
2024-02-20 10:05:58 -0500

    Controllers en Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,21 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+/**
+Route::get('/', [PageController::class, 'home'])->name('home');
 
-Route::get('/blog', function () {
-    // consulta a la base de datos
-    $posts = [
-        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
-        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
-    ];
+Route::get('/blog', [PageController::class, 'blog'])->name('blog');
 
-    return view('blog', ['posts' => $posts]);
-})->name('blog');
+Route::get('/blog/{word}', [PageController::class, 'post'])->name('post');
+ */
 
-Route::get('/blog/{word}', function ($word) {
-    // consulta a la base de datos
-    $post = $word;
-    return view('post', ['post' => $post]);
-})->name('post');
+Route::controller(PageController::class)->group(function () {
 
+    Route::get('/', 'home')->name('home');
+    Route::get('/blog', 'blog')->name('blog');
+    Route::get('/blog/{word}', 'post')->name('post');
+
+});
